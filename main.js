d4cac0a6bbeb93183e004590d6be4ebd2f20df65
ryanruri <muiruri13ryan@gmail.com>
2024-10-09 18:01:44 +0300

    initial git

--- main.js
+++ main.js
@@ -3,6 +3,34 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener('DOMContentLoaded', () => {
+  const hearts = document.querySelectorAll('.like-glyph');
+  const errorModal = document.getElementById('modal');
+  const errorMessage = document.getElementById('modal-message');
+
+  hearts.forEach(heart => {
+    heart.addEventListener('click', () => {
+      if (heart.textContent === EMPTY_HEART) {
+        mimicServerCall()
+          .then(() => {
+            heart.textContent = FULL_HEART;
+            heart.classList.add('activated-heart');
+          })
+          .catch((error) => {
+            errorMessage.textContent = error;
+            errorModal.classList.remove('hidden');
+            setTimeout(() => {
+              errorModal.classList.add('hidden');
+            }, 3000);
+          });
+      } else {
+        heart.textContent = EMPTY_HEART;
+        heart.classList.remove('activated-heart');
+      }
+    });
+  });
+});
+
 
 
 
@@ -23,3 +51,5 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+
+
